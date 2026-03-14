7bd430dd1949006ca8886b4bb9475d316ae9c627
kwnam0814 <kwnam0814@naver.com>
2026-03-14 14:42:17 +0900

    버튼 기능개발 + state변경

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,29 +5,35 @@ import "./App.css";
 const App = () => {
   let post = "강남 우동 맛집";
 
-  const [Title1, setTitle1] = useState("리액트 공부하기");
-  const [Title2, setTitle2] = useState("남자 정장 추천");
-  const [Title3, setTitle3] = useState("여자 정장 추천");
+  const [Title, setTitle] = useState([
+    "리액트 공부하기",
+    "남자 정장 추천",
+    "파이썬 독학",
+  ]);
+
   // 자주 변경될 것 같은 html 부분은 state로 만들어놓기
   // Title 자리 : "리액트 공부하기" 같은 자료가 들어있음
   // setTitle 자리 : state 변경을 도와주는 함수가 들어있음
 
-  // 배열 하나로 깔끔하게 관리하는 법
-  // const [titles, setTitles] = useState([
-  //   "리액트 공부하기",
-  //   "남자 정장 추천",
-  //   "여자 정장 추천",
-  // ]);
+  // 이런 식으로도 가능함
+  // const [Title1, setTitle1] = useState("리액트 공부하기");
+  // const [Title2, setTitle2] = useState("남자 정장 추천");
+  // const [Title3, setTitle3] = useState("여자 정장 추천");
 
-  // 화면에 뿌릴 때는?
-  // <h1>{titles[0]}</h1>
+  let [Like, setLike] = useState(0);
+
+  const [Gender, setGender] = useState("남자");
+
+  function Count() {
+    console.log(1);
+  }
 
   return (
     <>
       <div className="card">
         {/* class가 아니라 className */}
         <div className="black-nav">
-          <h2 id={post}>블로그!</h2>
+          <h2 id={post}>블로그 제목!</h2>
         </div>
         <div>
           {/* 변수 넣을 때(데이터바인딩)는 {중괄호} */}
@@ -40,22 +46,50 @@ const App = () => {
       </div>
       <div className="list card">
         <div>
-          <h2>{Title1}</h2>
+          <h2>
+            {Title[0]}{" "}
+            <span
+              onClick={() => {
+                setLike(Like + 1);
+              }}
+            >
+              👍
+            </span>{" "}
+            {Like}
+          </h2>
           <p>2월 17일 발행</p>
         </div>
 
         <hr />
 
         <div>
-          <h2>{Title2}</h2>
+          <h2>{Title[1]}</h2>
           <p>2월 18일 발행</p>
         </div>
 
         <hr />
 
         <div>
-          <h2>{Title3}</h2>
+          <h2>{Title[2]}</h2>
           <p>2월 19일 발행</p>
+        </div>
+
+        <hr />
+
+        <div>
+          <h2>
+            {Gender} 정장 추천{" "}
+            <button
+              className="button"
+              onClick={() => {
+                // Gender가 "남자"면 "여자"로 바꾸고, 아니면 "남자"로 바꿔라
+                setGender(Gender === "남자" ? "여자" : "남자");
+              }}
+            >
+              성별 바꾸기
+            </button>
+          </h2>
+          <p>2월 20일 발행</p>
         </div>
       </div>
     </>
