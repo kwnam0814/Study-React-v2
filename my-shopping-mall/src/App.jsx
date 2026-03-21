e94db550e258b88a5b0d91715e1f94ddb823a15d
kwnam0814 <kwnam0814@naver.com>
2026-03-21 23:07:02 +0900

    React 라우터1

--- my-shopping-mall/src/App.jsx
+++ my-shopping-mall/src/App.jsx
@@ -4,7 +4,8 @@ import { Button, Container, Nav, Navbar, Row, Col } from "react-bootstrap";
 import ShoesData from "./data";
 // import { 변수명1, 변수명2 } from "./data"; // 단, {} 안에는 export 했던 변수명 그대로 사용해야 함!
 
-import Shoes from "./components/shoes";
+import Details from "./components/Details";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
@@ -12,19 +13,33 @@ function App() {
       <div>
         <Navbar bg="light" data-bs-theme="light">
           <Container>
-            <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+            <Navbar.Brand href="/">ShoeShop</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/detail">상세페이지</Nav.Link>
               <Nav.Link href="#features">Features</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
+
+        <Link to="/">홈으로</Link>
+        <Link to="/detail">상세페이지</Link>
+
+        <div className="bg-[url(./img/bg-1.png)] h-[300px] bg-cover bg-center"></div>
       </div>
 
-      <div>
-        <Shoes />
-      </div>
+      <Routes>
+        <Route
+          path="/detail"
+          element={
+            <div>
+              <Details />
+            </div>
+          }
+        />
+        <Route path="/about" element={<div></div>} />
+      </Routes>
     </>
   );
 }
--- my-shopping-mall/src/components/Details.jsx
+++ my-shopping-mall/src/components/Details.jsx
@@ -7,7 +7,6 @@ const Shoes = () => {
   const [Shoes] = useState(ShoesData);
   return (
     <>
-      <div className="bg-[url(./img/bg-1.png)] h-[300px] bg-cover bg-center"></div>
       <Container>
         <Row>
           <Col>
@@ -17,12 +16,14 @@ const Shoes = () => {
             />
             <h4>{Shoes[0].title}</h4>
             <p>{Shoes[0].price}</p>
+            <button className="btn btn-danger">주문하기</button>
           </Col>
           <Col>
             {/* html에서 public폴더 이미지 사용할 때 권장방식(Vite 사용중일 때) */}
             <img src={import.meta.env.BASE_URL + "shoes2.jpg"} width="80%" />
             <h4>{Shoes[1].title}</h4>
             <p>{Shoes[1].price}</p>
+            <button className="btn btn-danger">주문하기</button>
           </Col>
           <Col>
             <img
@@ -31,6 +32,7 @@ const Shoes = () => {
             />
             <h4>{Shoes[2].title}</h4>
             <p>{Shoes[2].price}</p>
+            <button className="btn btn-danger">주문하기</button>
           </Col>
         </Row>
       </Container>
